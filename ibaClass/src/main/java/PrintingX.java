4175a321b29857c58b4a3b90c0e70caf200d1fa7
countmerloin <tahirdovtalabi@gmail.com>
2019-10-19 13:12:37 +0400

    -- edited PrintingX

--- ibaClass/src/main/java/PrintingX.java
+++ ibaClass/src/main/java/PrintingX.java
@@ -1,8 +1,9 @@
 public class PrintingX {
     public static void main(String[] args) {
-        for (int i = 1; i <= 10; i++ ) {
-            for (int j = 1; j <= 10; j++) {
-                if (i == 1 || i == 10 || j == 1 || j == 10 || i == j || ((i + j) == 11))
+        int n = 10;
+        for (int i = 1; i <= n; i++ ) {
+            for (int j = 1; j <= n; j++) {
+                if (i == 1 || i == 10 || j == 1 || j == 10 || i == j || ((i + j) == n + 1))
                     System.out.print("*");
                 else
                     System.out.print(" ");
