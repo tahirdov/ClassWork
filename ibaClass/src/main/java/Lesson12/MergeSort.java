1dd65fa96eb7b8e23cc2b7d5b1ccb6988319f30d
merloin <tahirdovtalabi@gmail.com>
2019-11-12 11:59:11 +0400

    - MergeSort

---
+++ ibaClass/src/main/java/Lesson12/MergeSort.java
@@ -0,0 +1,44 @@
+package Lesson12;
+
+import java.util.Arrays;
+
+public class MergeSort {
+
+    private static int[] merge(int[] a, int[] b) {
+        int length = a.length + b.length;
+        int[] merged = new int[length];
+        int i = 0, j = 0, k = 0;
+
+        while (a.length > i && b.length > j) {
+            if (a[i] < b[j]) {
+                merged[k] = a[i];
+                i++;
+            } else {
+                merged[k] = b[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (b.length > j) {
+            merged[k] = b[j];
+            j++;
+            k++;
+        }
+
+        while (a.length > i) {
+            merged[k] = a[i];
+            i++;
+            k++;
+        }
+        return merged;
+    }
+
+    public static void main(String[] args) {
+        int[] first = {4,5,6};
+        int[] second = {1,2,3};
+        int[] merged = merge(first, second);
+        System.out.println(Arrays.toString(merged));
+
+    }
+}
