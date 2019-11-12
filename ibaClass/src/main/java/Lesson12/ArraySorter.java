36db305d727d9f369aff858726c4ef26e3e9b5da
merloin <tahirdovtalabi@gmail.com>
2019-11-12 09:34:25 +0400

    - array list sort phase 1

---
+++ ibaClass/src/main/java/Lesson12/ArraySorter.java
@@ -0,0 +1,42 @@
+package Lesson12;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class ArraySorter {
+
+    static int[] sortArray(int[] array) {
+        int[] sorted = array.clone();
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                int temp = array[i];
+                if (array[i] < array[j]) {
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+        return sorted;
+ }
+
+    static int[] gen() {
+        Random random = new Random();
+        int[] array = new int[25];
+
+        for (int i = 0; i<25; i++) {
+            int ran = random.nextInt(41) + 10;
+            array[i] = ran;
+        }
+        return array;
+    }
+
+
+    public static void main(String[] args) {
+        int[] x = gen();
+        int[] y = sortArray(x);
+        System.out.printf("Unsorted array is: %s\n", Arrays.toString(x));
+        System.out.printf("Sorted array is: %s", Arrays.toString(y));
+    }
+
+
+}
